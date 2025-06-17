5f23dec42485597be705923b8b2afcb01b621fd1
Thushan Isuru <32395032+isuru072@users.noreply.github.com>
2025-06-17 17:14:05 +0800

    add serviceKey

--- services/firebase-service.js
+++ services/firebase-service.js
@@ -1,6 +1,10 @@
 import admin  from 'firebase-admin';
-import serviceAccount from '../serviceAccountKey.json' with { type: 'json' };
+//import serviceAccount from '../serviceAccountKey.json' with { type: 'json' };
 import fs from "fs";
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_KEY, 'base64').toString('utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
