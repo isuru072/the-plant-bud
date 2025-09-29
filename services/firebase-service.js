6ea4d342ce59a3f6740551075484acc8a9bbfe43
Thushan Isuru <>
2025-09-29 10:38:36 +0800

    minor

--- services/firebase-service.js
+++ services/firebase-service.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import serviceAccount from '../serviceAccountKey.json' with { type: 'json' };
+//import serviceAccount from '../serviceAccountKey.json' with { type: 'json' };
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
@@ -7,9 +7,9 @@ import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// const serviceAccount = JSON.parse(
-//   Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8")
-// );
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
